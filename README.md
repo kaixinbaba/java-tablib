f6837cefd22b2bc10a8d5ba79b6c22cda1e4e335
junjie.xun@oriente.com-at-120896031351 <junjie.xun@oriente.com-at-120896031351>
2019-02-12 16:02:13 +0800

    init project repo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # java-tablib
 Imitate tablib, Salute kennethreitz
+
+java-tablib is a format-agnostic tabular dataset library, written in Java.
---
+++ src/test/java/io/github/kaixinbaba/AppTest.java
@@ -0,0 +1,15 @@
+package io.github.kaixinbaba;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+
+    @Test
+    public void test() {
+        System.out.println("hello world");
+    }
+}
